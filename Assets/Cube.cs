3cb339579011ae101bce7dddeacd577d076f0987
Aleksandr <zabrodinalex67@gmail.com>
2024-04-14 02:49:43 +0900

    Cube prefab

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -3,17 +3,13 @@ using UnityEngine;
 [RequireComponent(typeof(CubeExplosion))]
 public class Cube : MonoBehaviour
 {
-    private CubeExplosion _cubeExplosion;
+    [SerializeField] private Cube _cube;
+    [SerializeField] private CubeExplosion _cubeExplosion;
 
     private int _sacaleRatio = 2;
     private Color[] _color = new Color[] { Color.green, Color.black, Color.blue, Color.red };
     private int _ratio = 200;
     private int _ratioChange = 2;
-
-    private void Start()
-    {
-        _cubeExplosion = GetComponent<CubeExplosion>();
-    }
 
     private void OnMouseUpAsButton()
     {
@@ -23,32 +19,27 @@ public class Cube : MonoBehaviour
     private void ExplodeAction()
     {
         int currentRatio = GetRandomRatio();
-        _ratio /= _ratioChange;
 
         _cubeExplosion.Explode();
         Destroy(gameObject);
+
         if (currentRatio <= _ratio)
         {
             CreateManually();
-            Debug.Log(_ratio);
-            Debug.Log(currentRatio);
         }
     }
 
     private void CreateManually()
     {
         int cubeNumber = GetRandomCubeNumber();
+        _ratio /= _ratioChange;
 
         for (int i = 0; i < cubeNumber; i++)
         {
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Cube cube = Instantiate(_cube, transform.position, Quaternion.identity);
             cube.GetComponent<Renderer>().material.color = _color[Random.Range(0, _color.Length)];
-            cube.AddComponent<BoxCollider>();
-            cube.AddComponent<Rigidbody>();
-            cube.AddComponent<Cube>();
-            cube.AddComponent<CubeExplosion>();
-            cube.transform.position = transform.position;
             cube.transform.localScale = transform.localScale / _sacaleRatio;
+            cube._ratio = _ratio;
         }
     }
 
